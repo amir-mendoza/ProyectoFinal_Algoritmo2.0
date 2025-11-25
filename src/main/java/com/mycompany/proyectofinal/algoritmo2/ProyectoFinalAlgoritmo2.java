57049172ecb4f675030ae85ccbeb93279bd61720
amir-mendoza <mendozallantays@gmail.com>
2025-11-24 21:34:27 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/proyectofinal/algoritmo2/ProyectoFinalAlgoritmo2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinal.algoritmo2;
+
+/**
+ *
+ * @author Acer
+ */
+public class ProyectoFinalAlgoritmo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
