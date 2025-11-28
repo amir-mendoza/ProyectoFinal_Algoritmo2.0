b8013ce3fd6083f0696ddea76957df5a0c12255e
amir-mendoza <mendozallantays@gmail.com>
2025-11-27 22:03:31 -0500

    Finalizado2

---
+++ src/main/java/com/mycompany/proyectofinal/algoritmo2/util/Catalogos.java
@@ -0,0 +1,34 @@
+package com.mycompany.proyectofinal.algoritmo2.util;
+
+public class Catalogos {
+
+    public static final String[] CATEGORIAS = {
+        "Abarrotes",
+        "Bebidas",
+        "Limpieza",
+        "Enlatados",
+        "Snacks"
+    };
+
+    public static final String[] PROVEEDORES = {
+        "Alicorp",
+        "Gloria",
+        "San Fernando",
+        "Nestlé",
+        "PepsiCo"
+    };
+
+    public static void mostrarCategorias() {
+        System.out.println("Categorías:");
+        for (int i = 0; i < CATEGORIAS.length; i++) {
+            System.out.println(i + " → " + CATEGORIAS[i]);
+        }
+    }
+
+    public static void mostrarProveedores() {
+        System.out.println("Proveedores:");
+        for (int i = 0; i < PROVEEDORES.length; i++) {
+            System.out.println(i + " → " + PROVEEDORES[i]);
+        }
+    }
+}
